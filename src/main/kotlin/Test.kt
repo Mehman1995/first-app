de625369a2446e367d0d41def7744e3e6a226c66
Mehman1995 <87534806+Mehman1995@users.noreply.github.com>
2021-08-08 00:57:04 +0300

    Дз№2 задача2

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -4,8 +4,4 @@ fun main() {
     val creditCard = 35 / 100
     val amount = 0.35 * 0.75
     println("$creditCard рублей $amount копеек")
-
-
-
-
 }
---
+++ src/main/kotlin/Test2.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val like = 61
+
+    if (like < 60 || like > 100  ) {
+        println("Понравилось людям")
+    } else {
+        println("Понравилось человеку")
+    }
+}
