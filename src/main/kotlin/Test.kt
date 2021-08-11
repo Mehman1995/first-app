027c0541a8744cac7aa9747ded4defb49da176a9
Mehman1995 <87534806+Mehman1995@users.noreply.github.com>
2021-08-11 14:40:10 +0300

    Дз№2 задача1 переделал

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,8 +1,11 @@
-fun main() {
-
-
-    val creditCard = 35 / 100
-    val amount = 0.35 * 0.75
-    println("$creditCard рублей $amount копеек")
-
-}
+ fun main() {
+        val sum = 77000.0
+        val comission = 0.75
+        val minimalComission = 35
+        val remmitance = if (sum < 75_000) {
+            sum
+        } else {
+            sum - (sum * comission / 100) - minimalComission
+        }
+        println("Перевод с учетом комиссии: $remmitance руб.")
+    }
