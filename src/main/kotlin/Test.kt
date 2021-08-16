019d29fe71e18af38f4f5cadf45c63405101ba2b
Mehman1995 <87534806+Mehman1995@users.noreply.github.com>
2021-08-16 12:48:56 +0300

    Дз№2 задача1 переделал

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,12 +1,12 @@
 fun main() {
 
-    val sum = 77000.0
-    val comission = 0.75
-    val minimalComission = 35
-    val remmitance = if (sum < 75_000) {
-        sum
-    } else {
-        sum - (sum * comission / 100) - minimalComission
+    val minCommission = 3500
+    val percentCommission = 0.75f
+    val amount= 500000
+    var result = (amount / 100) * percentCommission
+
+    if (result < minCommission) {
+        result = minCommission.toFloat()
     }
-    println("Перевод с учетом комиссии: $remmitance руб.")
+    println(result / 100)
 }
